2ae337773f520d303b8af9a78252560dcb5e8650
Minaht007 <talmresaerch@gmail.com>
2023-07-11 15:38:18 +0300

    mainPageBody Style

--- src/pages/main/mainPage.jsx
+++ src/pages/main/mainPage.jsx
@@ -1,22 +1,28 @@
 import React from "react";
-
 import { Link, Outlet } from "react-router-dom";
+import Style from "./mainPage.module.scss";
 
 const MainPage = () => {
   return (
-    <>
+    <div className={Style.linkContainer}>
       <Link to="/clientPage">
-        <button type="button">If you need help</button>
+        <button className={Style.linkButton} type="button">
+          <span className={Style.textButton}> If you need help</span>
+        </button>
       </Link>
 
       <Link to="/SignUpForm">
-        <button type="button">
-          If you can help <br /> doctors and admins
+        <button className={Style.linkButton} type="button">
+          <span className={Style.textButton}>
+            {" "}
+            If you can help <br />{" "}
+            <span className={Style.textButtonAdd}> (doctors and admins)</span>
+          </span>
         </button>
       </Link>
 
       <Outlet />
-    </>
+    </div>
   );
 };
 
