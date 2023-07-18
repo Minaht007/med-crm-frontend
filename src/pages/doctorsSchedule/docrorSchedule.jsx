505e0b5ff23372c636459a14506b158aa7fcba9f
Minaht007 <talmresaerch@gmail.com>
2023-07-19 00:52:34 +0300

    while comment

--- src/pages/doctorsSchedule/docrorSchedule.jsx
+++ src/pages/doctorsSchedule/docrorSchedule.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import moment from "moment";
 
 const DoctoreSchedulle = () => {
-  window.moment = moment;
+  // window.moment = moment;
   moment.updateLocale("en", { week: { dow: 1 } });
   const startDay = moment().startOf("month").startOf("week");
   const endDay = moment().endOf("month").endOf("week");
@@ -11,14 +11,14 @@ const DoctoreSchedulle = () => {
   console.log(startDay.format("DD.MM.YYYY"));
   console.log(endDay.format("DD.MM.YYYY"));
 
-  const calendar = [];
-  console.log(calendar);
-  const day = startDay;
+  // const calendar = [];
+  // console.log(calendar);
+  // const day = startDay;
 
-  while (!day.isSame(endDay)) {
-    calendar.push(day);
-    day.add(1, "day");
-  }
+  // while (!day.isSame(endDay)) {
+  //   calendar.push(day);
+  //   day.add(1, "day");
+  // }
 
   return <div></div>;
 };
