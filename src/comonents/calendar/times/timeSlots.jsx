a91e0a347c08c2b81bf1593b9e17c1901ea9f19a
Minaht007 <talmresaerch@gmail.com>
2023-07-19 13:58:00 +0300

    table styly

--- src/comonents/calendar/times/timeSlots.jsx
+++ src/comonents/calendar/times/timeSlots.jsx
@@ -1,33 +1,35 @@
 import React from "react";
 import { addMinutes, format } from "date-fns";
+import styles from "./timeSlots.module.scss"; // Import the styles file
 
 const TimeSlots = () => {
   const startTime = new Date();
-  startTime.setHours(1, 0, 0); // Начало временных слотов в 9:00 AM
+  startTime.setHours(0, 0, 0); // Start time for time slots at 00:00
 
   const endTime = new Date();
-  endTime.setHours(23, 0, 0); // Конец временных слотов в 3:00 PM
+  endTime.setHours(23, 40, 0); // End time for time slots at 23:40
 
-  const step = 20; // Шаг временных слотов в 20 минут
-
-  const timeSlots = [];
+  const step = 20; // Time slot interval in minutes
 
   let currentTime = startTime;
+  const timeSlots = [];
+
   while (currentTime <= endTime) {
-    const formattedTime = format(currentTime, "h:mm aa");
-    timeSlots.push(formattedTime);
+    const formattedTime = format(currentTime, "HH:mm");
+    const isHour = formattedTime.endsWith(":00");
+
+    timeSlots.push(
+      <tr key={formattedTime}>
+        <td className={isHour ? styles.hour : styles.minute}>
+          {formattedTime}
+        </td>
+      </tr>
+    );
+
     currentTime = addMinutes(currentTime, step);
   }
 
-  return (
-    <React.Fragment>
-      {timeSlots.map((timeSlot, index) => (
-        <tr key={index}>
-          <td>{timeSlot}</td>
-        </tr>
-      ))}
-    </React.Fragment>
-  );
+  return <tbody>{timeSlots}</tbody>;
 };
 
 export default TimeSlots;
